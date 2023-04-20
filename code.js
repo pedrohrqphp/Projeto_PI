e57809445261b68f559b7375dc0248021ba34c50
_ <85962372+Nickolas120@users.noreply.github.com>
2023-04-20 21:07:53 +0000

    Implementado sistema básico de login

--- code.js
+++ code.js
@@ -1,1 +1,29 @@
+function matriz(){
+    var dados = [
+        {id:1, login:"Will", senha:1315},
+        {id:2, login:"Black", senha:1111},
+        {id:3, login:"Sandra", senha:2020}    
+    ]
 
+    return dados
+
+}
+function login(email,pass){
+
+    const db = matriz()
+    
+    var encontrado = false;
+    for (var i = 0; i < db.length; i++) {
+      if (db[i].login == email && db[i].senha == pass) {
+        encontrado = true;
+        break;
+      }
+    }
+    if (encontrado == false) {
+        alert("Email ou senha inválidos");
+        
+      } else {
+        alert("Login bem-sucedido!");
+      }
+    
+}
